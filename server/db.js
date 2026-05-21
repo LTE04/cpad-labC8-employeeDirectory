b04ba853f6c7c34fc174ce9567b4ee6c8ec3eb74
LTE04 <leetungern@graduate.utm.my>
2026-05-21 13:09:45 +0800

    add server/db.js

---
+++ server/db.js
@@ -0,0 +1,15 @@
+// server/db.js
+const mysql = require('mysql2/promise')
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'employee_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
+
+module.exports = pool
